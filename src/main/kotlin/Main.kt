62bf9f3c5e0f19b921106cb658438f9948396350
Chris Jones <chris@cjmobileapps.com>
2024-10-13 01:27:46 -0500

    Print page size : part 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -17,12 +17,12 @@ fun main(args: Array<String>) {
         val databaseFile = java.io.File(databaseFilePath).inputStream()
 
         // Uncomment this block to pass the first stage
-        //
-        // databaseFile.skip(16) // Skip the first 16 bytes of the header
-        // val pageSizeBytes = ByteArray(2) // The following 2 bytes are the page size
-        // databaseFile.read(pageSizeBytes)
-        // val pageSize = ByteBuffer.wrap(pageSizeBytes).short.toUShort()
-        // println("database page size: $pageSize")
+
+         databaseFile.skip(16) // Skip the first 16 bytes of the header
+         val pageSizeBytes = ByteArray(2) // The following 2 bytes are the page size
+         databaseFile.read(pageSizeBytes)
+         val pageSize = ByteBuffer.wrap(pageSizeBytes).short.toUShort()
+         println("database page size: $pageSize")
 
         databaseFile.close()
     } else {
